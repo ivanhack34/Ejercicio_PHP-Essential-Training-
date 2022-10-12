92dc86cf491cbf15f45ba036539dcb62d40dd863
ivanhack34 <ivanpolanco34@gmail.com>
2022-10-12 12:18:00 -0400

    Mis ejercicio del curso

---
+++ Ritmo del curso/Capitulo 2/sandbox/helloworld.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+  <head>
+    <title>Hello World</title>
+  </head>
+  <body>
+    <?php
+    // single-line comments are like this
+    # or like this (less common)
+    /* doble-line comments are written
+        like this, so that can keep typing
+        an typing*/
+    ?>
+    <?php echo "Hello World!"; ?><br />
+    <?php echo "Hello" . " World!"; ?><br />
+    <?php echo 2 + 3 ?>
+  </body>
+</html>
